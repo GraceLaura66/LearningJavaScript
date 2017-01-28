d68798977fb1070238a1f5c6739baf73966318fd
lgrozas6 <lauragracep@live.com>
2017-01-27 21:36:57 -0600

    contional statements
    contional statements

---
+++ Conditional Statements/main.js
@@ -0,0 +1,11 @@
+var answer = prompt('What programming language is the name of this gem?');
+
+
+if ( answer === 'Ruby')
+{
+ document.write("<p>That's right!</p>");
+}
+ else
+ {
+   document.write("<p>Sorry, wrong answer.</p>");
+ }
