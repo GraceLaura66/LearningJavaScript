d5e41dfd4651399c5abb9a9892d6408983638188
lgrozas6 <lauragracep@live.com>
2017-01-30 14:46:38 -0600

    conditions
    conditions

--- Conditional Statements/main.js
+++ Conditional Statements/main.js
@@ -9,3 +9,19 @@ if ( answer === 'Ruby')
  {
    document.write("<p>Sorry, wrong answer.</p>");
  }
+
+
+
+// comparing answers example
+
+ if ( a > b ) {
+   alert('a is greater than b')
+ }
+
+ else {
+     alert(' a is NOT greater than b')
+   };
+
+
+var randomNumber = Math.floor(Math.random() * 6 ) + 1;
+var guess = prompt('I am thinking of a number between 1 and 6. What is it?')
