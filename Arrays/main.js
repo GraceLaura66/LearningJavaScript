9f3bc503157bab2f2c4640b0ac2f01139266a603
lgrozas6 <lauragracep@live.com>
2017-02-03 13:59:16 -0600

    arrays
    arrays

--- Arrays/main.js
+++ Arrays/main.js
@@ -1,24 +1,54 @@
-function randomNumber(upper) {
-  return Math.floor( Math.random() * upper ) + 1;
+var randomNumber = getRandomNumber(10);
+var guess;
+var guessCount = 0;
+
+function getRandomNumber( upper ) {
+  var num = Math.floor(Math.random() * upper) + 1;
+  return num;
 }
-var counter = 0;
-while ( counter < 1000 ) {
-  var randNum = randomNumber(6);
-  document.write(randNum + '');
-  counter += 1;
-}
-
-
-
-
-
-document.write(count);
 
 
 
 
 
 
+
+
+
+
+// var upper = 10000;
+// var randomNumber = getRandomNumber(upper);
+// var guess;
+// var attempts = 0;
+//
+// function getRandomNumber(upper) {
+//   return Math.floor( Math.random() * upper ) + 1;
+// }
+//
+// while ( guess !== randomNumber ) {
+//   guess = getRandomNumber( upper );
+//   attempts += 1;
+// }
+// document.write("<p>The random number was: " + randomNumber +"</p>")
+// document.write("<p> It took the computer " + attempts + " attempts to get it right.</p>")
+
+
+
+// function randomNumber(upper) {
+//   return Math.floor( Math.random() * upper ) + 1;
+// }
+// var counter = 0;
+// while ( counter < 1000 ) {
+//   var randNum = randomNumber(6);
+//   document.write(randNum + '');
+//   counter += 1;
+// }
+//
+//
+//
+//
+//
+// document.write(count);
 
 // while () {
 //
